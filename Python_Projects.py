a70b2ab04ccbf9e0f54f948ece4bfb7d5105467d
AleGermosen <agermosenb@gmail.com>
2024-11-27 12:59:21 -0400

    QR Code Generator

--- Python_Projects.py
+++ Python_Projects.py
@@ -114,7 +114,7 @@ def guess_number():
 # Make it a 2 out of 3 wins
 # Count how many wins the player has
 # Add a choice for two players
-
+"""
 import random
 
 def r_p_s_game():
@@ -188,10 +188,53 @@ def r_p_s_game():
         print(f"Player 2 won {count_wins_player_2} times.")
     else:
         print("Wrong choice, run program again.")
+"""
+
+### QR Code Generator
+
+# Enter list to generate codes
+# Choose color for the code
+# Enter filename
+# Save code as an image
+
+import qrcode
+
+def qr_code_generator():
+    prompt_text = input("Enter a list of items separated by commas: ")
+    prompt_text = prompt_text.split(",")
+    total_items = len(prompt_text)
+    
+    prompt_filename = (input("Enter the base filename: "))
+    prompt_color_1 = (input("Enter primary color: ")).lower()
+    prompt_color_2 = (input("Enter secondary color: ")).lower()
+
+    count_items = 0
+    while (count_items < total_items):
+        # Create a QR Code object
+        qr = qrcode.QRCode(
+            version=1,  # Controls the size of the QR Code (1 is smallest)
+            error_correction=qrcode.constants.ERROR_CORRECT_L,  # Error correction level
+            box_size=10,  # Size of each box in pixels
+            border=4,  # Thickness of the border (minimum is 4)
+        )
+
+        # Add data to the QR Code
+        qr.add_data(prompt_text[count_items])
+        qr.make(fit=True)
+
+        # Create an image of the QR Code
+        img = qr.make_image(fill_color=prompt_color_1, back_color=prompt_color_2)
+
+        # Save the QR Code image
+        img.save(f"{prompt_filename}_{count_items}.png")
+        print(f"QR Code saved as '{prompt_filename}_{count_items}.png'")
+
+        count_items += 1
 
 
 if __name__ == '__main__': 
     # roll_dice()
     # roll_dice_enhanced()
     # guess_number()
-    r_p_s_game()
+    # r_p_s_game()
+    qr_code_generator()
