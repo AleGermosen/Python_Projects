eca82f9c15a3e41c94d02b7a6062fa9961a5d1a1
AleGermosen <agermosenb@gmail.com>
2024-11-28 17:47:16 -0400

    Currency Converter

--- Python_Projects.py
+++ Python_Projects.py
@@ -196,7 +196,7 @@ def r_p_s_game():
 # Choose color for the code
 # Enter filename
 # Save code as an image
-
+"""
 import qrcode
 
 def qr_code_generator():
@@ -230,6 +230,54 @@ def qr_code_generator():
         print(f"QR Code saved as '{prompt_filename}_{count_items}.png'")
 
         count_items += 1
+"""
+
+### Currency Converter
+
+# # User imputs:
+# Amount to convert
+# Source and Target currency from given options
+# Fixed conversion from source to target
+# Display currency converted in multiple currencies
+# Display a history of conversions
+
+nested_dict = {
+    "USD": {'EUR': 0.95, 'CAD': 1.40, 'GBP': 0.79},
+    "EUR": {'USD': 1.06, 'CAD': 0.68, 'GBP': 0.83},
+    "CAD": {'USD': 0.71, 'EUR': 1.48, 'GBP': 0.56},
+    "GBP": {'USD': 1.27, 'EUR': 1.20, 'CAD': 1.78}
+}
+
+def get_user_input():
+    amount_to_convert = float(input("Enter the amount: "))
+    source_currency = (input("(USD/EUR/CAD/GBP): ")).upper()
+    target_currency = (input("(USD/EUR/CAD/GBP), separated with commas: ")).upper()
+    target_currency = target_currency.split(",")
+    return amount_to_convert, source_currency, target_currency
+
+
+def convert_to_currency(d, amount_to_convert, source_currency, target_currency):    
+    conversion = []
+    
+    count_items = 0
+    total_items = len(target_currency)
+    while (count_items < total_items):
+        target = d[source_currency][target_currency[count_items]]
+        convert = amount_to_convert * target
+        conversion.append(convert)
+        count_items += 1
+    return conversion
+
+def currency_converter():
+    user_amount, user_source, user_target = get_user_input()
+    user_conversion = convert_to_currency(nested_dict, user_amount, user_source, user_target)
+    
+    count_items = 0
+    total_items = len(user_target)
+    while (count_items < total_items):
+        converted_currency = user_conversion[count_items]
+        print(f"{user_amount} {user_source} is equal to {converted_currency} {user_target[count_items]}")
+        count_items += 1
 
 
 if __name__ == '__main__': 
@@ -237,4 +285,5 @@ if __name__ == '__main__':
     # roll_dice_enhanced()
     # guess_number()
     # r_p_s_game()
-    qr_code_generator()
+    # qr_code_generator()
+    currency_converter()
