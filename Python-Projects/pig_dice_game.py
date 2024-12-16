fd5ca76bdfa20b7a0597824111e8b9ff5262f80a
AleGermosen <agermosenb@gmail.com>
2024-12-16 11:07:02 -0400

    Pig Dice Game

--- Python-Projects/pig_dice_game.py
+++ Python-Projects/pig_dice_game.py
@@ -15,29 +15,43 @@ def roll_dice():
     dice = random.randint(1, 6)
     print(f"You rolled a {dice}")
     current_score_list.append(dice)
-    return sum(current_score_list)
+    if dice == 1:
+        current_score_list.clear()
+        return 0
+    else:
+        return sum(current_score_list)
 
 def pig_dice_game():
     amount_players = 2 # int(input("How many players are going to play? "))
+    amount_points = 20 # int(input("Up to how many points are you going to play? "))
+    
     players_score = {key: 0 for key in range(amount_players)}
+    final_score = {key: 0 for key in range(amount_players)}
 
-    for key in players_score:
-        print(f"Player {key + 1}'s turn")
-        current_score = roll_dice()
-        while True:
-            answer = input("Roll the dice? (y/n): ").upper()
-            if answer == "Y":
-                current_score = roll_dice()
-            elif answer == "N":
-                try: 
-                    players_score.update({key: current_score})
-                except UnboundLocalError:
-                    current_score = dice
-                print(f"Player {key + 1}'s previous score was {current_score}")
-                current_score_list.clear()
-                break
-            else:
-                print("Invalid choice!")
-
-
-# pig_dice_game()
+    while True:
+        for key in players_score:
+            print(f"Player {key + 1}'s turn")
+            current_score = roll_dice()
+            
+            while dice != 1:
+                answer = input("Roll the dice? (y/n): ").upper()
+                if answer == "Y":
+                    current_score = roll_dice()
+                elif answer == "N":
+                    try: 
+                        players_score.update({key: current_score})
+                    except UnboundLocalError:
+                        current_score = dice
+                    print(f"Player {key + 1}'s previous score was {current_score}")
+                    current_score_list.clear()
+                    break
+                else:
+                    print("Invalid choice!")
+            if dice == 1:
+                players_score[key] = 0
+            final_score[key] += players_score[key]
+            if final_score[key] >= amount_points:
+                print(f"Player {key + 1} has won with a final score of {final_score[key]}")
+                exit()
+                    
+        print(f"Player 1's current total score is {final_score[0]} \nPlayer 2's current total score is {final_score[1]}")
